733a055e90efb25d858eaa29c603e188596d39ee
RSThakur <rakeshthkaur348@gmail.com>
2014-08-21 22:37:19 -0700

    Create KernalCuda.cu

---
+++ KernalCuda.cu
@@ -0,0 +1,114 @@
+/* Source Code Nvdia Cuda Kit*/
+// includes, system
+#include <stdio.h>
+#include <assert.h>
+
+// Simple utility function to check for CUDA runtime errors
+void checkCUDAError(const char *msg);
+
+// Part 3 of 5: implement the kernel
+__global__ void myFirstKernel(int *d_a)
+{
+    int idx = blockIdx.x*blockDim.x + threadIdx.x;
+    d_a[idx] = 1000*blockIdx.x + threadIdx.x;  
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Program main
+////////////////////////////////////////////////////////////////////////////////
+int main( int argc, char** argv) 
+{
+
+	//time event variables definition
+	cudaEvent_t start, stop, kstart, kstop;
+	float timeElapsed,ktimeElapsed;
+	cudaEventCreate(&start);
+	cudaEventCreate(&kstart);
+	cudaEventCreate(&stop);
+	cudaEventCreate(&kstop);
+	
+    // pointer for host memory
+    int *h_a;
+
+    // pointer for device memory
+    int *d_a;
+
+    // define grid and block size
+	int numBlocks = atoi(argv[1]);
+    int numThreadsPerBlock = atoi(argv[2]);
+    //int numBlocks = 8;
+    //int numThreadsPerBlock = 8;
+
+	//Start cuda timings
+	cudaEventRecord(start,0);
+	cudaEventRecord(kstart,0);
+    // Part 1 of 5: allocate host and device memory
+    size_t memSize = numBlocks * numThreadsPerBlock * sizeof(int);
+    h_a = (int *) malloc(memSize);
+    cudaMalloc( (void **) &d_a, memSize );
+	
+		
+    // Part 2 of 5: launch kernel
+    dim3 dimGrid(numBlocks);
+    dim3 dimBlock(numThreadsPerBlock);
+    myFirstKernel<<< dimGrid, dimBlock >>>( d_a );
+
+    // block until the device has completed
+    cudaThreadSynchronize();
+	cudaEventRecord(kstop,0);
+	cudaEventSynchronize(kstop);
+	cudaEventElapsedTime(&ktimeElapsed,kstart,kstop);
+
+    // check if kernel execution generated an error
+    checkCUDAError("kernel execution");
+
+    // Part 4 of 5: device to host copy
+    cudaMemcpy( h_a, d_a, memSize, cudaMemcpyDeviceToHost );
+
+    // Check for any CUDA errors
+    checkCUDAError("cudaMemcpy");
+
+    // Part 5 of 5: verify the data returned to the host is correct
+    for (int i = 0; i < numBlocks; i++)
+    {
+        for (int j = 0; j < numThreadsPerBlock; j++)
+        {
+            assert(h_a[i * numThreadsPerBlock + j] == 1000 * i + j);
+        }
+    }
+
+    // free device memory
+    cudaFree(d_a);
+
+    // free host memory
+    free(h_a);
+
+	//Stop cuda timings
+	
+	cudaEventRecord(stop,0);
+	cudaEventSynchronize(stop);
+	
+	cudaEventElapsedTime(&timeElapsed,start,stop);
+	
+	//destroy cuda events
+	cudaEventDestroy(start);
+	cudaEventDestroy(stop);
+	printf("Time taken by kernal = %0.2f msecs\n", ktimeElapsed);
+	printf("Time taken for execution = %0.2f msecs\n",timeElapsed);
+
+    // If the program makes it this far, then the results are correct and
+    // there are no run-time errors.  Good work!
+    printf("Correct!\n");
+	
+    return 0;
+}
+
+void checkCUDAError(const char *msg)
+{
+    cudaError_t err = cudaGetLastError();
+    if( cudaSuccess != err) 
+    {
+        fprintf(stderr, "Cuda error: %s: %s.\n", msg, cudaGetErrorString( err) );
+        exit(-1);
+    }                         
+}
